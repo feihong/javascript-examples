6d4703bd470a130d6b11e0397f4197b86da35de7
Feihong Hsu <feihong.hsu@gmail.com>
2015-12-01 14:56:40 -0600

    Add lodash library

--- lib-bundle/build.sh
+++ lib-bundle/build.sh
@@ -1,3 +1,4 @@
-browserify -r react -r react-dom -r react-addons-update -r radium \
-  -r underscore.string \
+browserify \
+  -r lodash -r underscore.string \
+  -r react -r react-dom -r react-addons-update -r radium \
   -o ../files/lib/lib-bundle.js
